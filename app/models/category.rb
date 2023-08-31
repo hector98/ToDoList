d261d2c887774ca347d2461c5e782e485cb4c3eb
Hector Manuel Barrios Barrios <hector.barrios@realitat.com.mx>
2023-08-30 21:39:47 -0600

    Trabajando en agragar tareas

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,21 +1,9 @@
 class Category < ApplicationRecord
-
-  def to_css_color
-    case color
-    when 'red'
-      'red-500'
-    when 'blue'
-      'blue-500'
-    else
-      'green-500'
-    end
-  end
-
   enum color: {
     red: 0,
     blue: 1,
     green: 4
   }
 
-  has_many :task
+  has_many :tasks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :task
-  resources :categories
+  resources :categories do
+    resources :tasks
+  end
 end
